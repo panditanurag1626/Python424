6ebc423fd2386912f69f97a3ebe06153a70bb145
panditanurag1626 <panditanurag638@gmail.com>
2026-03-09 09:30:57 +0000

    first commit

--- Day15 string and patter/pattern.py
+++ Day15 string and patter/pattern.py
@@ -1,2 +1,12 @@
+# for i in range(3):
+#     print("*")
+# r=3
+# for i in range(1,r+1):
+#     for j in range(1,i+1):
+#         print("*",end="")
+#     print()
+
 for i in range(3):
-    print("*")
+    for j in range(3):
+        print("*",end="")
+    print()
