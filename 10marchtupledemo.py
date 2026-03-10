79adc8f459cdc7ef08fa400d6adfb39f17d5af97
panditanurag1626 <panditanurag638@gmail.com>
2026-03-10 09:04:37 +0000

    upto tuple1

---
+++ 10marchtupledemo.py
@@ -0,0 +1,15 @@
+# Create tuple
+tup=()
+print(tup,type,(tup))
+t1=tuple()
+print(t1,type(t1))
+
+t2=(500)
+print(t2,type(t2))
+
+t3=(500,100,"hello",True,5j,89.8789)
+print(t3,type(t3))
+
+# t4=tuple(78,89,90) #error
+t4=tuple((78,89,90)) 
+print(t4,type(t4))
